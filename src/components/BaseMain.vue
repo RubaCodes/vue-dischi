8b2b15d3f4bdc7542d175aed9dd44a029ace1af9
rubacodes <rubacodes@gmail.com>
2022-06-07 15:04:28 +0200

    aggiunta library section

--- src/components/BaseMain.vue
+++ src/components/BaseMain.vue
@@ -1,9 +1,13 @@
 <template>
-  <main class="h-full bg-slate-800"></main>
+  <main class="h-full bg-slate-800">
+    <LibrarySection></LibrarySection>
+  </main>
 </template>
 
 <script>
+import LibrarySection from './sections/LibrarySection.vue';
 export default {
+  components: { LibrarySection },
   name: 'BaseMain',
 };
 </script>
---
+++ src/components/sections/LibrarySection.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates mollitia
+    necessitatibus quibusdam perspiciatis laboriosam eius nihil explicabo nisi.
+    Tempore illo ipsum sequi nobis aspernatur eaque? Error perferendis rem
+    expedita repudiandae.
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LibrarySection',
+};
+</script>
