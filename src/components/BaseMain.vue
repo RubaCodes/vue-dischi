58aaca87f9eeedc1995bab78666a1b80c86077eb
rubacodes <rubacodes@gmail.com>
2022-06-07 14:49:18 +0200

    creati header e main , aggiunto logo spotify

---
+++ src/components/BaseMain.vue
@@ -0,0 +1,11 @@
+<template>
+  <header>
+    <img src="../assets/img/spotify-logo.png" alt="logo-spotify" />
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'BaseHeader',
+};
+</script>
