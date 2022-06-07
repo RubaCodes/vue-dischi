522eac1f88c4acf4f7ac7f27c2b2d818b73190a8
rubacodes <rubacodes@gmail.com>
2022-06-07 15:59:20 +0200

    dato h fisse pe ravere tutto one page

--- src/components/BaseMain.vue
+++ src/components/BaseMain.vue
@@ -1,6 +1,6 @@
 <template>
-  <main class="bg-slate-800">
-    <LibrarySection class="w-5/6 md:w-3/4 mx-auto py-11"></LibrarySection>
+  <main class="bg-slate-800 h-[full - 74px]">
+    <LibrarySection class="w-5/6 md:w-3/4 mx-auto py-12"></LibrarySection>
   </main>
 </template>
 
--- src/components/sections/LibrarySection.vue
+++ src/components/sections/LibrarySection.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="grid grid-cols-1 md:grid-cols-5 gap-4">
+  <div class="grid grid-cols-1 md:grid-cols-5 gap-8">
     <AlbumCard
       v-for="album in albums"
       :key="album.id"
