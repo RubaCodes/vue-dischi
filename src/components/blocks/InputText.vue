fa7ba7bf03f607430ba364367c511cac92a7a5ed
rubacodes <rubacodes@gmail.com>
2022-06-08 15:06:59 +0200

    aggiunto componente input text

---
+++ src/components/blocks/InputText.vue
@@ -0,0 +1,12 @@
+<template>
+  <form>
+    <input type="text" placeholder="Cerca Artista..." />
+    <button>Submit</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'InputText',
+};
+</script>
--- src/components/sections/LibrarySection.vue
+++ src/components/sections/LibrarySection.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="grid grid-cols-1 md:grid-cols-3 xl:grid-cols-5 gap-x-8 gap-y-4">
-    <AlbumCard
-      v-for="album in albums"
-      :key="album.id"
-      :album="album"
-    ></AlbumCard>
+  <div>
+    <InputText></InputText>
+    <div class="grid grid-cols-1 md:grid-cols-3 xl:grid-cols-5 gap-x-8 gap-y-4">
+      <AlbumCard
+        v-for="album in albums"
+        :key="album.id"
+        :album="album"
+      ></AlbumCard>
+    </div>
   </div>
 </template>
 
 <script>
 import AlbumCard from '../blocks/AlbumCard.vue';
+
 import axios from 'axios';
+import InputText from '../blocks/InputText.vue';
 export default {
-  components: { AlbumCard },
+  components: { AlbumCard, InputText },
   name: 'LibrarySection',
   data() {
     return {
