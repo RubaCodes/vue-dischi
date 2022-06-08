96ea8ef38e51c08de76b739fb614c8b054440746
rubacodes <rubacodes@gmail.com>
2022-06-08 15:44:59 +0200

    creazione select

---
+++ src/components/blocks/InputSelect.vue
@@ -0,0 +1,20 @@
+<template>
+  <select>
+    <option value=""></option>
+    <option value=""></option>
+    <option value=""></option>
+    <option value=""></option>
+    <option value=""></option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: 'InputText',
+  data() {
+    return {
+      searchText: '',
+    };
+  },
+};
+</script>
