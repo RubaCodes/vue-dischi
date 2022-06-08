8427a1592f99371fd01b162173ef3e934d21c550
rubacodes <rubacodes@gmail.com>
2022-06-08 17:42:35 +0200

    fix overflow-y

--- src/components/BaseMain.vue
+++ src/components/BaseMain.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="bg-slate-800 h-[calc(100vh-66px)]">
+  <main class="bg-slate-800 h-[calc(100vh-66px)] overflow-y-auto">
     <LibrarySection class="w-5/6 md:w-3/5 mx-auto py-14"></LibrarySection>
   </main>
 </template>
